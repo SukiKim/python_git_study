56db98f3b8424ae07e35cc93706acee5b0ac977d
SukiKim <comnat12@naver.com>
2024-05-07 22:09:46 +0900

    Add myself.jpg file and push button with handler function in main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox 
+# QMessageBox: 메세지박스 위젯
+from PyQt5.QtGui import QIcon # Icon 추가를 위한 라이브러리
+
 class Calculator(QWidget):
 
     def __init__(self):
@@ -8,9 +11,25 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Calculator')
-        self.resize(256,256)
+        self.btn1 = QPushButton('Message', self) # 버튼추가
+        self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭시 핸들러 함수 연결
+
+        vbox  = QVBoxLayout() #수직 레이아웃 위젯 생성
+        vbox.addStretch(1) # 빈공간
+        vbox.addWidget(self.btn1) # 버튼 위치
+        vbox.addStretch(1) # 빈공간
+
+        self.setLayout(vbox) # 빈공간 - 버튼
+
+
+        self.setWindowTitle('Calculator312312')
+        self.setWindowIcon(QIcon('myself.jpg'))
+        self.resize(256, 256)
         self.show()
+
+    def activateMessage(self): # 버튼을 클릭할 때 동작하는 함수: 메세지 박스 출력
+        QMessageBox.information(self,"information", "button clicked!")
+
 if __name__=='__main__':
     app = QApplication(sys.argv)
     view = Calculator()
