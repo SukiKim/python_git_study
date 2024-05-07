4389c736f9ff9c9414cb0a2a69f94701f08a5266
SukiKim <comnat12@naver.com>
2024-05-07 22:14:03 +0900

    Add test files, test.txt and test2.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox 
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit
 # QMessageBox: 메세지박스 위젯
 from PyQt5.QtGui import QIcon # Icon 추가를 위한 라이브러리
 
@@ -11,10 +11,14 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
+
         self.btn1 = QPushButton('Message', self) # 버튼추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭시 핸들러 함수 연결
 
         vbox  = QVBoxLayout() #수직 레이아웃 위젯 생성
+        vbox.addWidget(self.te1)
         vbox.addStretch(1) # 빈공간
         vbox.addWidget(self.btn1) # 버튼 위치
         vbox.addStretch(1) # 빈공간
