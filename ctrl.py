035fd93eb1e6991043968008fc33036567218af4
SukiKim <comnat12@naver.com>
2024-05-27 18:48:51 +0900

    Modify ctrl.py to connect sum operation to display

--- ctrl.py
+++ ctrl.py
@@ -1,11 +1,19 @@
-# 6.4.4 ctrl.py
+# 6.6.2 ctrl.py
 class Control:
     def __init__(self, view):
         self.view = view
         self.connectSignals()
         
     def calculate(self):
-        pass
+        num1 = float(self.view.le1.text())
+        num2 = float(self.view.le2.text())
+        operator =self.view.cb.currentText()
+        
+        if operator =='+':
+            return f'{num1} + {num2} = {self.sum(num1, num2)}'
+        
+        else:
+            return "Calculation Error"
         
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.calculate)
@@ -16,10 +24,11 @@ class Control:
             return a+b
         except:
             return "Calculation Error"
+        
 
 
-
-# 6.4.1 ctrl.py
+        
+# # 6.4.4 ctrl.py
 # class Control:
 #     def __init__(self, view):
 #         self.view = view
@@ -33,24 +42,45 @@ class Control:
 #         self.view.btn2.clicked.connect(self.view.clearMessage)
         
 #     def sum(self, a, b):
-#        return a+b
+#         try:
+#             return a+b
+#         except:
+#             return "Calculation Error"
 
-# class Control:
-#     def __init__(self, view):
-#         self.view = view
-#         self.connectSignals()
+
+
+# # 6.4.1 ctrl.py
+# # class Control:
+# #     def __init__(self, view):
+# #         self.view = view
+# #         self.connectSignals()
         
-#     def connectSignals(self):
-#         self.view.btn1.clicked.connect(self.view.activateMessage)
-#         self.view.btn2.clicked.connect(self.view.clearMessage)
+# #     def calculate(self):
+# #         pass
+        
+# #     def connectSignals(self):
+# #         self.view.btn1.clicked.connect(self.calculate)
+# #         self.view.btn2.clicked.connect(self.view.clearMessage)
+        
+# #     def sum(self, a, b):
+# #        return a+b
+
+# # class Control:
+# #     def __init__(self, view):
+# #         self.view = view
+# #         self.connectSignals()
+        
+# #     def connectSignals(self):
+# #         self.view.btn1.clicked.connect(self.view.activateMessage)
+# #         self.view.btn2.clicked.connect(self.view.clearMessage)
 
 
-# ch 5.2.1 ctrl.pyversions
-#class Control:
-#    def __init__(self, view):
-#        self.view = view
-#        self.connectSignals()
+# # ch 5.2.1 ctrl.pyversions
+# #class Control:
+# #    def __init__(self, view):
+# #        self.view = view
+# #        self.connectSignals()
 
-#    def connectSignals(self):
-#        self.view.btn1.clicked.connect(self.view.activateMessage)
-#        self.view.btn2.clicked.connect(self.view.clearMessage)
+# #    def connectSignals(self):
+# #        self.view.btn1.clicked.connect(self.view.activateMessage)
+# #        self.view.btn2.clicked.connect(self.view.clearMessage)
--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-# 6.6.1 ui.py
+# 6.6.7 ui.py
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
                              QLineEdit, QComboBox)
@@ -59,11 +59,7 @@ class View(QWidget):
     def clearMessage(self):
         self.te1.clear()
 
-# # <<<<<<< HEAD
-# # 6.3.3 ui.py
-# # =======
-# # 6.3.1 ui.py
-# # >>>>>>> ca089ef8608fbd7999a2ce8e65fb8caf1b54fa9e
+# # 6.6.1 ui.py
 # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
 #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
 #                              QLineEdit, QComboBox)
@@ -93,7 +89,7 @@ class View(QWidget):
 #         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
 #         self.cb = QComboBox(self)
-#         self.cb.addItems(['+', '-', '*', '/'])
+#         self.cb.addItems(['+', '-', '*', '/', '^'])
         
 #         hbox_formular = QHBoxLayout()
 #         hbox_formular.addWidget(self.le1)
@@ -124,126 +120,11 @@ class View(QWidget):
 #     def clearMessage(self):
 #         self.te1.clear()
 
-
-# # 6.3.2 ui.py
-# # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-# #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
-# #                              QLineEdit, QComboBox)
-# # from PyQt5.QtGui import QIcon
-# # from PyQt5 import QtCore
-
-
-# # class View(QWidget):
-    
-# #     def __init__(self):
-# #         super().__init__()
-# #         self.initUI()
-        
-# #     def initUI(self):
-# #         self.te1 = QPlainTextEdit()
-# #         self.te1.setReadOnly(True)
-        
-# #         self.btn1=QPushButton('Message',self)
-# #         self.btn2=QPushButton('Clear',self)
-        
-# #         self.le1=QLineEdit('0',self)
-# #         self.le1.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.le2=QLineEdit('0',self)
-# #         self.le2.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.cb = QComboBox(self)
-# #         self.cb.addItems(['+', '-', '*', '/'])
-        
-# #         hbox_formular = QHBoxLayout()
-# #         hbox_formular.addWidget(self.le1)
-# #         hbox_formular.addWidget(self.cb)
-# #         hbox_formular.addWidget(self.le2)
-        
-# #         hbox = QHBoxLayout()
-# #         hbox.addStretch(1)
-# #         hbox.addWidget(self.btn1)
-# #         hbox.addWidget(self.btn2)
-        
-# #         vbox=QVBoxLayout()
-# #         vbox.addWidget(self.te1)
-# #         vbox.addLayout(hbox_formular)
-# #         vbox.addLayout(hbox)
-# #         vbox.addStretch(1)
-        
-# #         self.setLayout(vbox)
-        
-# #         self.setWindowTitle('Calculator')
-# #         self.setWindowIcon(QIcon('icon.png'))
-# #         self.resize(256,256)
-# #         self.show()
-        
-# #     def activateMessage(self, text):
-# #         self.te1.appendPlainText(text)
-        
-# #     def clearMessage(self):
-# #         self.te1.clear()
-
-# # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-# #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
-# #                              QLineEdit, QComboBox)
-# # from PyQt5.QtGui import QIcon
-# # from PyQt5 import QtCore
-# # <<<<<<< HEAD
-# # =======
-
-
-# # class View(QWidget):
-    
-# #     def __init__(self):
-# #         super().__init__()
-# #         self.initUI()
-        
-# #     def initUI(self):
-# #         self.te1 = QPlainTextEdit()
-# #         self.te1.setReadOnly(True)
-        
-# #         self.btn1=QPushButton('Message',self)
-# #         self.btn2=QPushButton('Clear',self)
-        
-# #         self.le1=QLineEdit('0',self)
-# #         self.le1.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.le2=QLineEdit('0',self)
-# #         self.le2.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.cb = QComboBox(self)
-# #         self.cb.addItems(['+', '-', '*', '/'])
-        
-# #         hbox_formular = QHBoxLayout()
-# #         hbox_formular.addWidget(self.le1)
-# #         hbox_formular.addWidget(self.cb)
-# #         hbox_formular.addWidget(self.le2)
-        
-# #         hbox = QHBoxLayout()
-# #         hbox.addStretch(1)
-# #         hbox.addWidget(self.btn1)
-# #         hbox.addWidget(self.btn2)
-        
-# #         vbox=QVBoxLayout()
-# #         vbox.addWidget(self.te1)
-# #         vbox.addLayout(hbox_formular)
-# #         vbox.addLayout(hbox)
-# #         vbox.addStretch(1)
-        
-# #         self.setLayout(vbox)
-        
-# #         self.setWindowTitle('Calculator')
-# #         self.setWindowIcon(QIcon('icon.png'))
-# #         self.resize(256,256)
-# #         self.show()
-        
-# #     def activateMessage(self, text):
-# #         self.te1.appendPlainText(text)
-        
-# #     def clearMessage(self):
-# #         self.te1.clear()
-
+# # # <<<<<<< HEAD
+# # # 6.3.3 ui.py
+# # # =======
+# # # 6.3.1 ui.py
+# # # >>>>>>> ca089ef8608fbd7999a2ce8e65fb8caf1b54fa9e
 # # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
 # #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
 # #                              QLineEdit, QComboBox)
@@ -298,67 +179,131 @@ class View(QWidget):
 # #         self.resize(256,256)
 # #         self.show()
         
-# #     def setDisplay(self):
-# #         self.te1.appendPlainText("Button clicked!")
-        
-# #     def clearMessage(self):
-# #         self.te1.clear()
-
-# # # ch 5.2.1 ui.py
-# # #from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
-# # >>>>>>> ca089ef8608fbd7999a2ce8e65fb8caf1b54fa9e
-
-
-# # class View(QWidget):
-    
-# #     def __init__(self):
-# #         super().__init__()
-# #         self.initUI()
-        
-# #     def initUI(self):
-# #         self.te1 = QPlainTextEdit()
-# #         self.te1.setReadOnly(True)
-        
-# #         self.btn1=QPushButton('Message',self)
-# #         self.btn2=QPushButton('Clear',self)
-        
-# #         self.le1=QLineEdit('0',self)
-# #         self.le1.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.le2=QLineEdit('0',self)
-# #         self.le2.setAlignment(QtCore.Qt.AlignRight)
-        
-# #         self.cb = QComboBox(self)
-# #         self.cb.addItems(['+', '-', '*', '/'])
-        
-# #         hbox_formular = QHBoxLayout()
-# #         hbox_formular.addWidget(self.le1)
-# #         hbox_formular.addWidget(self.cb)
-# #         hbox_formular.addWidget(self.le2)
-        
-# #         hbox = QHBoxLayout()
-# #         hbox.addStretch(1)
-# #         hbox.addWidget(self.btn1)
-# #         hbox.addWidget(self.btn2)
-        
-# #         vbox=QVBoxLayout()
-# #         vbox.addWidget(self.te1)
-# #         vbox.addLayout(hbox_formular)
-# #         vbox.addLayout(hbox)
-# #         vbox.addStretch(1)
-        
-# #         self.setLayout(vbox)
-        
-# #         self.setWindowTitle('Calculator')
-# #         self.setWindowIcon(QIcon('icon.png'))
-# #         self.resize(256,256)
-# #         self.show()
-        
-# #     def activateMessage(self, text):
+# #     def setDisplay(self, text):
 # #         self.te1.appendPlainText(text)
         
 # #     def clearMessage(self):
 # #         self.te1.clear()
+
+
+# # # 6.3.2 ui.py
+# # # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+# # #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
+# # #                              QLineEdit, QComboBox)
+# # # from PyQt5.QtGui import QIcon
+# # # from PyQt5 import QtCore
+
+
+# # # class View(QWidget):
+    
+# # #     def __init__(self):
+# # #         super().__init__()
+# # #         self.initUI()
+        
+# # #     def initUI(self):
+# # #         self.te1 = QPlainTextEdit()
+# # #         self.te1.setReadOnly(True)
+        
+# # #         self.btn1=QPushButton('Message',self)
+# # #         self.btn2=QPushButton('Clear',self)
+        
+# # #         self.le1=QLineEdit('0',self)
+# # #         self.le1.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.le2=QLineEdit('0',self)
+# # #         self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.cb = QComboBox(self)
+# # #         self.cb.addItems(['+', '-', '*', '/'])
+        
+# # #         hbox_formular = QHBoxLayout()
+# # #         hbox_formular.addWidget(self.le1)
+# # #         hbox_formular.addWidget(self.cb)
+# # #         hbox_formular.addWidget(self.le2)
+        
+# # #         hbox = QHBoxLayout()
+# # #         hbox.addStretch(1)
+# # #         hbox.addWidget(self.btn1)
+# # #         hbox.addWidget(self.btn2)
+        
+# # #         vbox=QVBoxLayout()
+# # #         vbox.addWidget(self.te1)
+# # #         vbox.addLayout(hbox_formular)
+# # #         vbox.addLayout(hbox)
+# # #         vbox.addStretch(1)
+        
+# # #         self.setLayout(vbox)
+        
+# # #         self.setWindowTitle('Calculator')
+# # #         self.setWindowIcon(QIcon('icon.png'))
+# # #         self.resize(256,256)
+# # #         self.show()
+        
+# # #     def activateMessage(self, text):
+# # #         self.te1.appendPlainText(text)
+        
+# # #     def clearMessage(self):
+# # #         self.te1.clear()
+
+# # # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+# # #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
+# # #                              QLineEdit, QComboBox)
+# # # from PyQt5.QtGui import QIcon
+# # # from PyQt5 import QtCore
+# # # <<<<<<< HEAD
+# # # =======
+
+
+# # # class View(QWidget):
+    
+# # #     def __init__(self):
+# # #         super().__init__()
+# # #         self.initUI()
+        
+# # #     def initUI(self):
+# # #         self.te1 = QPlainTextEdit()
+# # #         self.te1.setReadOnly(True)
+        
+# # #         self.btn1=QPushButton('Message',self)
+# # #         self.btn2=QPushButton('Clear',self)
+        
+# # #         self.le1=QLineEdit('0',self)
+# # #         self.le1.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.le2=QLineEdit('0',self)
+# # #         self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.cb = QComboBox(self)
+# # #         self.cb.addItems(['+', '-', '*', '/'])
+        
+# # #         hbox_formular = QHBoxLayout()
+# # #         hbox_formular.addWidget(self.le1)
+# # #         hbox_formular.addWidget(self.cb)
+# # #         hbox_formular.addWidget(self.le2)
+        
+# # #         hbox = QHBoxLayout()
+# # #         hbox.addStretch(1)
+# # #         hbox.addWidget(self.btn1)
+# # #         hbox.addWidget(self.btn2)
+        
+# # #         vbox=QVBoxLayout()
+# # #         vbox.addWidget(self.te1)
+# # #         vbox.addLayout(hbox_formular)
+# # #         vbox.addLayout(hbox)
+# # #         vbox.addStretch(1)
+        
+# # #         self.setLayout(vbox)
+        
+# # #         self.setWindowTitle('Calculator')
+# # #         self.setWindowIcon(QIcon('icon.png'))
+# # #         self.resize(256,256)
+# # #         self.show()
+        
+# # #     def activateMessage(self, text):
+# # #         self.te1.appendPlainText(text)
+        
+# # #     def clearMessage(self):
+# # #         self.te1.clear()
 
 # # # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
 # # #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
@@ -422,42 +367,158 @@ class View(QWidget):
 
 # # # # ch 5.2.1 ui.py
 # # # #from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
+# # # >>>>>>> ca089ef8608fbd7999a2ce8e65fb8caf1b54fa9e
 
-# # # #from PyQt5.QtGui import QIcon
 
-# # # #class View(QWidget):
+# # # class View(QWidget):
+    
+# # #     def __init__(self):
+# # #         super().__init__()
+# # #         self.initUI()
+        
+# # #     def initUI(self):
+# # #         self.te1 = QPlainTextEdit()
+# # #         self.te1.setReadOnly(True)
+        
+# # #         self.btn1=QPushButton('Message',self)
+# # #         self.btn2=QPushButton('Clear',self)
+        
+# # #         self.le1=QLineEdit('0',self)
+# # #         self.le1.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.le2=QLineEdit('0',self)
+# # #         self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+# # #         self.cb = QComboBox(self)
+# # #         self.cb.addItems(['+', '-', '*', '/'])
+        
+# # #         hbox_formular = QHBoxLayout()
+# # #         hbox_formular.addWidget(self.le1)
+# # #         hbox_formular.addWidget(self.cb)
+# # #         hbox_formular.addWidget(self.le2)
+        
+# # #         hbox = QHBoxLayout()
+# # #         hbox.addStretch(1)
+# # #         hbox.addWidget(self.btn1)
+# # #         hbox.addWidget(self.btn2)
+        
+# # #         vbox=QVBoxLayout()
+# # #         vbox.addWidget(self.te1)
+# # #         vbox.addLayout(hbox_formular)
+# # #         vbox.addLayout(hbox)
+# # #         vbox.addStretch(1)
+        
+# # #         self.setLayout(vbox)
+        
+# # #         self.setWindowTitle('Calculator')
+# # #         self.setWindowIcon(QIcon('icon.png'))
+# # #         self.resize(256,256)
+# # #         self.show()
+        
+# # #     def activateMessage(self, text):
+# # #         self.te1.appendPlainText(text)
+        
+# # #     def clearMessage(self):
+# # #         self.te1.clear()
 
-# # # #    def __init__(self):
-# # # #        super().__init__()
-# # # #        self.initUI()
+# # # # from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+# # # #                              QMessageBox, QPlainTextEdit, QHBoxLayout,
+# # # #                              QLineEdit, QComboBox)
+# # # # from PyQt5.QtGui import QIcon
+# # # # from PyQt5 import QtCore
 
-# # # #    def initUI(self):
-# # # #        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
-# # # #        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
 
-# # # #        self.btn1 = QPushButton('Message', self) # 버튼추가
-# # # #        self.btn2 = QPushButton('Clear', self) # 버튼 클릭시 핸들러 함수 연결
+# # # # class View(QWidget):
+    
+# # # #     def __init__(self):
+# # # #         super().__init__()
+# # # #         self.initUI()
+        
+# # # #     def initUI(self):
+# # # #         self.te1 = QPlainTextEdit()
+# # # #         self.te1.setReadOnly(True)
+        
+# # # #         self.btn1=QPushButton('Calc',self)
+# # # #         self.btn2=QPushButton('Clear',self)
+        
+# # # #         self.le1=QLineEdit('0',self)
+# # # #         self.le1.setAlignment(QtCore.Qt.AlignRight)
+# # # #         self.le1.setFocus(True)
+# # # #         self.le1.selectAll()
+        
+# # # #         self.le2=QLineEdit('0',self)
+# # # #         self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+# # # #         self.cb = QComboBox(self)
+# # # #         self.cb.addItems(['+', '-', '*', '/'])
+        
+# # # #         hbox_formular = QHBoxLayout()
+# # # #         hbox_formular.addWidget(self.le1)
+# # # #         hbox_formular.addWidget(self.cb)
+# # # #         hbox_formular.addWidget(self.le2)
+        
+# # # #         hbox = QHBoxLayout()
+# # # #         hbox.addStretch(1)
+# # # #         hbox.addWidget(self.btn1)
+# # # #         hbox.addWidget(self.btn2)
+        
+# # # #         vbox=QVBoxLayout()
+# # # #         vbox.addWidget(self.te1)
+# # # #         vbox.addLayout(hbox_formular)
+# # # #         vbox.addLayout(hbox)
+# # # #         vbox.addStretch(1)
+        
+# # # #         self.setLayout(vbox)
+        
+# # # #         self.setWindowTitle('Calculator')
+# # # #         self.setWindowIcon(QIcon('icon.png'))
+# # # #         self.resize(256,256)
+# # # #         self.show()
+        
+# # # #     def setDisplay(self):
+# # # #         self.te1.appendPlainText("Button clicked!")
+        
+# # # #     def clearMessage(self):
+# # # #         self.te1.clear()
 
-# # # #        hbox  = QHBoxLayout()
-# # # #        hbox.addStretch(1) 
-# # # #        hbox.addWidget(self.btn1)
-# # # #        hbox.addWidget(self.btn2)
+# # # # # ch 5.2.1 ui.py
+# # # # #from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
 
-# # # #        vbox = QVBoxLayout()
+# # # # #from PyQt5.QtGui import QIcon
 
-# # # #       vbox.addWidget(self.te1)
-# # # #        vbox.addLayout(hbox)
-# # # #        vbox.addStretch(1)
-# # # #
-# # # #        self.setLayout(vbox) # 빈공간 - 버튼
+# # # # #class View(QWidget):
 
-# # # #        self.setWindowTitle('Calculator')
-# # # #        # self.setWindowIcon(QIcon('myself.jpg'))
-# # # #        self.resize(256, 256)
-# # # #        self.show()
+# # # # #    def __init__(self):
+# # # # #        super().__init__()
+# # # # #        self.initUI()
 
-# # # #    def activateMessage(self):
-# # # #        self.te1.appendPlainText("Button clicked!")
+# # # # #    def initUI(self):
+# # # # #        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+# # # # #        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
 
-# # # #    def clearMessage(self):
-# # # #        self.te1.clear()
+# # # # #        self.btn1 = QPushButton('Message', self) # 버튼추가
+# # # # #        self.btn2 = QPushButton('Clear', self) # 버튼 클릭시 핸들러 함수 연결
+
+# # # # #        hbox  = QHBoxLayout()
+# # # # #        hbox.addStretch(1) 
+# # # # #        hbox.addWidget(self.btn1)
+# # # # #        hbox.addWidget(self.btn2)
+
+# # # # #        vbox = QVBoxLayout()
+
+# # # # #       vbox.addWidget(self.te1)
+# # # # #        vbox.addLayout(hbox)
+# # # # #        vbox.addStretch(1)
+# # # # #
+# # # # #        self.setLayout(vbox) # 빈공간 - 버튼
+
+# # # # #        self.setWindowTitle('Calculator')
+# # # # #        # self.setWindowIcon(QIcon('myself.jpg'))
+# # # # #        self.resize(256, 256)
+# # # # #        self.show()
+
+# # # # #    def activateMessage(self):
+# # # # #        self.te1.appendPlainText("Button clicked!")
+
+# # # # #    def clearMessage(self):
+# # # # #        self.te1.clear()
